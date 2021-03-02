2c0815ff90dc3aa82aa23f2eba6124ef79b5bbde
STÉFFANY CAROLINE DA SILVA <79550342+steffanysi@users.noreply.github.com>
2021-03-02 19:21:04 -0300

    Update Main.java
    Atividade de Configuração da plataforma de Programação

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, my name is ....");
+    System.out.println("Olá, eu sou Stéffany e moro em Olímpia - SP.");
   }
 }
