33e110e19cf7098021957eab1ef7fa14e6c995de
STÉFFANY CAROLINE DA SILVA <79550342+steffanysi@users.noreply.github.com>
2021-03-02 20:23:47 -0300

    Update main.java
    Correção

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Ola, eu sou Stéffany e moro em Olimpia - SP");
+    System.out.println("Ola, eu sou Stéffany e moro em Olimpia - SP.");
   }
 }
